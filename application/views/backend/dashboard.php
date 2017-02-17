53501514b6edaec9efbd815fa718e00d586f36db
Chintan <chintan@wohlig.com>
2017-02-17 19:47:41 +0530

    done

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -1,11 +1,6 @@
 
 <h4 class="pad-left-15 capitalize">Upload Home Banner</h4>
-<div class="btn blue darken-4">
-<span>Image</span>
+<?php echo form_open_multipart('site/uploadSiteBannerImage');?>
 <input type="file" name="image"/>
-</div>
-<div class="file-path-wrapper">
-    <input class="file-path validate image1" type="text" placeholder="Upload one or more files" value="<?php echo form_open_multipart('site/uploadSiteBannerImage');?>">
-</div>
-<input class="btn btn-primary waves-effect waves-light  blue darken-4" type="submit" value="upload" />
+<button class="btn btn-primary waves-effect waves-light  blue darken-4" type="submit" />Upload</button>
 </form>
