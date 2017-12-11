1a76eae14d48a7a23b127123eca1df2f37227cfe
Kishori Bihade <kishori.bihade@gmail.com>
2017-12-11 17:28:31 +0530

    dashboard

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -1,25 +1,10 @@
 <div class="container-fluid">
     <div class="file-field input-field col m6 s12">
-        <h4 class="pad-left-15 capitalize">Upload Home Banner </h4>
-        	<span class="img-center big image1">
-                   			<?php if ($before->image == '') {
-								} else {
-									?><img src="<?php echo base_url('uploads').'/'.$before->image;
-									?>">
-														<?php
-								} ?></span>
+        <h4 class="pad-left-15 capitalize">Upload Home Banner</h4>
         <?php echo form_open_multipart('site/uploadSiteBannerImage');?>
-        <input type="file" name="image" value="<?php echo set_value('image', $before->image);?>"/>
+        <input type="file" name="image"/>
         <button class="btn btn-primary waves-effect waves-light  blue darken-4" type="submit" />Upload</button>
-        <p style=" display: block;
-		padding-top: 30px;">Size (900x1277)</p>
         </form>
+        <p style=" display: block;padding-top: 30px;">Size (900x1277)</p>
     </div>
 </div>
-<h4 class="pad-left-15 capitalize">Upload Home Banner</h4>
-<?php echo form_open_multipart('site/uploadSiteBannerImage');?>
-<input type="file" name="image"/>
-<button class="btn btn-primary waves-effect waves-light  blue darken-4" type="submit" />Upload</button>
-</form>
-<p style=" display: block;
-		padding-top: 30px;">Size (900x1277)</p>
