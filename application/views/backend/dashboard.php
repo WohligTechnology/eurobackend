fecf658de5d315314d29dc90fc8380aee9ed8617
Kishori Bihade <kishori.bihade@gmail.com>
2017-12-11 17:40:24 +0530

    dashboard

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -1,6 +1,6 @@
-        <h4 class="pad-left-15 capitalize">Upload Home Banner</h4>
+        <h4 class="pad-left-15 capitalize">Upload Home Banner  <span style="font-size:16px;">Size (900x1277)</span></h4>
         <?php echo form_open_multipart('site/uploadSiteBannerImage');?>
         <input type="file" name="image"/>
         <button class="btn btn-primary waves-effect waves-light  blue darken-4" type="submit" />Upload</button>
         </form>
-        <p style=" display: block;padding-top: 30px;">Size (900x1277)</p>
+       
